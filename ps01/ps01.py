6519b06a23e2289dd684ace9e9c89d8fd9577f0d
Dmytro Shaban <dmytro.shaban@gmail.com>
2022-05-25 22:20:21 +0200

    PS01. Random keys.

--- ps01/ps01.py
+++ ps01/ps01.py
@@ -1,3 +1,5 @@
+import secrets
+
 def variants(length):
     '''Returns the number of variants of a bit sequence with length is "length"'''
     return 2 ** length
@@ -5,9 +7,17 @@ def variants(length):
 
 if __name__ == '__main__':
     key_lengths = [8, 16, 32, 64, 128, 256, 512, 1024, 4069]
+    # Part 1. Variants of keys.
     print('Part 1')
     print()
     for length in key_lengths:
         print(f'For {length} bit key we have {variants(length)} possible keys.')
+    print()
+    # Part 2. Random keys
+    print('Part 2')
+    print()
+    keys = [secrets.randbits(length) for length in key_lengths]
+    for length, key in zip(key_lengths, keys):
+        print(f'{length} bit key is {key}')
 
 
